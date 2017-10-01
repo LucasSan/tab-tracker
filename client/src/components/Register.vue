95a97e6a8de802078ccdabd176c7158c66652a41
Lucas Santos <lucas.santos@mereo.com>
2017-10-01 20:10:43 -0300

    master client

---
+++ client/src/components/Register.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input v-model="email" type="text" name="email" placeholder="email" />
+    <br />
+    <input v-model="password" type="password" name="password" placeholder="password" />
+    <br/>
+    <button
+    @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  },
+  watch: {
+    email (value) {
+      console.log('email has change: ', value)
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this.email = 'hello world'
+    }, 1000)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
